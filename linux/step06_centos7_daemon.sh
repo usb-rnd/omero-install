6ab878b1167ad866959281636c89183fd5ea97c1
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-07-05 22:03:09 +0100

    remove remaining ref to centos

--- linux/step06_centos7_daemon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#start-recommended
-cp omero-server-systemd.service /etc/systemd/system/omero-server.service
-if [ ! -f /.dockerenv ]; then
-    systemctl daemon-reload
-fi
-systemctl enable omero-server.service
-#end-recommended
